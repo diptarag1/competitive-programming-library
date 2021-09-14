62b309335dbac85604f3d1335e2b6a813b0a26cd
Diptarag <diptarag1@gmail.com>
2021-09-14 23:24:04 +0530

    Matrix changes and templating sieve

--- Numbers/Linear Sieve.cpp
+++ Numbers/Linear Sieve.cpp
@@ -1,11 +1,14 @@
-int N = 1e8;
-vector<int> sieve(N+1, 1), primes;
-void primepre() {
-	for(int i = 2; i <= N; ++i) {
-		if(sieve[i]) primes.pb(i);
-		for(int j = 0; j < primes.size() && i*primes[j] <= N; ++j) {
-			sieve[i*primes[j]] = 0;
-			if(i%primes[j]==0) break;
-		}
-	}
-}
+template<int n = 1000000>
+struct lsieve {
+    vector<int> sieve, primes;
+    lsieve() {
+        sieve.resize(n+1, 1);
+        for(int i = 2; i <= n; ++i) {
+            if(sieve[i]) primes.pb(i);
+            for(int j = 0; j < primes.size() && i*primes[j] <= n; ++j) {
+                sieve[i*primes[j]] = 0;
+                if(i%primes[j]==0) break;
+            }
+        }
+    }
+};
--- Numbers/MatrixStruct.cpp
+++ Numbers/MatrixStruct.cpp
@@ -6,15 +6,18 @@ struct Matrix {
         matrix = vector<vector<T>>(rows, vector<T>(cols));
         if(def) loop(i,0,rows) matrix[i][i] = def;
     }
-    Matrix(Matrix&& a) : matrix(move(a.matrix)) { }
+    Matrix(vector<vector<T>>&& a) : matrix(a) { rows = matrix.size(); cols = matrix[0].size(); }
+    Matrix(Matrix&& a) : matrix(move(a.matrix)) { rows = matrix.size(); cols = matrix[0].size(); }
+    void operator = (Matrix&& a) { matrix = move(a.matrix); rows = matrix.size(); cols = matrix[0].size(); }
     friend Matrix operator * (Matrix<T>& a, Matrix<T>& b) {
         assert(a.cols == b.rows);
         Matrix<T> ans(a.rows, b.cols);
         loop(i,0,a.rows) loop(j,0,b.cols) loop(k,0,a.cols) ans.matrix[i][j] += a.matrix[i][k]*b.matrix[k][j];
         return ans;
     }
-    friend Matrix operator ^ (Matrix<T> a, ll b) {
-        Matrix<T> res(a.rows, a.cols, 1); 
+    friend Matrix operator ^ (Matrix<T>& aa, ll b) {
+        Matrix<T> res(aa.rows, aa.cols, 1); 
+        Matrix<T> a = move(aa);
         for (; b; b >>= 1) 
         { if (b & 1) res = res * a; a = a * a; }
         return res;
