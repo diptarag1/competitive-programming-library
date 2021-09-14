1c9278c46a46f59d2f96b3fbf34c8bcd0cb89df6
Diptarag <diptarag1@gmail.com>
2021-09-15 00:56:30 +0530

    Structed segtree

--- SegTree/SegTree (Range Query + Assignment).cpp
+++ SegTree/SegTree (Range Query + Assignment).cpp
@@ -1,80 +1,80 @@
-int lg = 20;
-int N = 1<<(lg-1);
-
+//Data node
 struct node{
-    ll a;
-    node(ll aa=0) : a(aa) {}
-};
-struct lnode {
-    ll mult, add;
-    lnode(ll aa=1, ll bb=0) : mult(aa), add(bb) {}
-};
-
-node merge(const node& n1, const node& n2) {
-    return node(addm(n1.a, n2.a));
-}
-node merge1(const node& n1, const lnode& n2, int siz) {
-    return node( addm(multm(n2.mult,n1.a),multm(n2.add,siz)) );
-}
-lnode merge2(const lnode& n1, const lnode& n2) {
-    return lnode( multm(n1.mult,n2.mult), addm(multm(n1.add,n2.mult),n2.add) ); 
-}
-
-template<typename T, typename U>
-void modify(int pos, vector<T>& tree, vector<U>& lazy, U val) {
-    int siz = 1<<(lg-1-__lg(pos));
-    tree[pos] = merge1(tree[pos], val,siz);
-    if(pos < N) lazy[pos] = merge2(lazy[pos], val);
-}
-
-//Initialzies tree
-template<typename T>
-inline void init(vector<T>& tree) {
-    for(int i = N-1; i > 0; i--) tree[i] = merge(tree[i<<1], tree[i<<1|1]);
-}
-
-//Builds all parents of pos from bottom
-template<typename T, typename U>
-inline void build(int pos, vector<T>& tree, vector<U>& lazy) {
-    for(pos+=N, pos>>=1; pos; pos>>=1) { 
-        int siz = 1<<(lg-1-__lg(pos)); 
-        tree[pos] = merge(tree[pos<<1], tree[pos<<1|1]); tree[pos] = merge1(tree[pos], lazy[pos], siz);
+    int l,r; mint a;
+    node(int l=0, int r=0, mint aa=0) : l(l), r(r), a(aa) {}
+    friend node merge(const node& n1, const node& n2) {
+        return node(n1.l, n2.r, n1.a + n2.a);
     }
-}
+};
 
-//Pushes lazy of all parents of L from top
-template<typename T, typename U>
-void push(int l, vector<T>& tree, vector<U>& lazy) {
-    for(int s = lg; s > 0; --s) {
-        int pos = l>>s;
-        if(lazy[pos].mult != 1 || lazy[pos].add) { 
-            modify(pos<<1,tree,lazy,lazy[pos]); 
-            modify(pos<<1|1,tree,lazy,lazy[pos]); 
-            lazy[pos]= {1,0}; 
+//Update node
+struct lnode {
+    mint mult, add;
+    lnode(mint aa=1, mint bb=0) : mult(aa), add(bb) {}
+    friend node merge1(const node& n1, const lnode& n2) {
+        return node( n1.l, n1.r, n2.mult*n1.a + n2.add*(n1.r - n1.l + 1) );
+    }
+    friend lnode merge2(const lnode& n1, const lnode& n2) {
+        return lnode( n1.mult*n2.mult, n1.add*n2.mult+n2.add ); 
+    }
+};
+
+
+template<typename T, typename U> 
+struct SegTree {
+    int N, lg; vector<T> tree; vector<U> lazy;
+    SegTree(int n, vector<T>& t, vector<U>& l) : tree(move(t)), lazy(move(l)), N(n) { 
+        lg = 1; ll c = 1; 
+        while(c < n) { c<<=1; lg++; } 
+        for(int i = N-1; i > 0; i--) tree[i] = merge(tree[i<<1], tree[i<<1|1]);
+    }
+
+    void modify(int pos, U val) {
+        tree[pos] = merge1(tree[pos], val);
+        if(pos < N) lazy[pos] = merge2(lazy[pos], val);
+    }
+
+    //Builds all parents of pos from bottom
+    inline void build(int pos) {
+        for(pos+=N, pos>>=1; pos; pos>>=1) { 
+            tree[pos] = merge(tree[pos<<1], tree[pos<<1|1]); tree[pos] = merge1(tree[pos], lazy[pos]);
         }
     }
-}
 
-//Queries [L,R)
-template<typename T, typename U>
-T query(int l, int r, vector<T>& tree, vector<U>& lazy) {
-    push(l+N,tree,lazy); push(r-1+N,tree,lazy);
-    T res;
-    for(l+=N, r+=N; l < r; l>>=1, r>>=1) {
-        if(l&1) res = merge(res, tree[l++]);
-        if(r&1) res = merge(res, tree[--r]);
-    } 
-    return res;
-}
+    //Pushes lazy of all parents of L from top
+    void push(int l) {
+        for(int s = lg; s > 0; --s) {
+            int pos = l>>s;
+            if(lazy[pos].mult != 1 || lazy[pos].add != 0) { 
+                modify(pos<<1,lazy[pos]); 
+                modify(pos<<1|1,lazy[pos]); 
+                lazy[pos]= {1,0}; 
+            }
+        }
+    }
 
-//Updates [L,R)
-template<typename T, typename U>
-void update(int l, int r, U val, vector<T>& tree, vector<U>& lazy) {
-    push(l+N,tree,lazy); push(r-1+N,tree,lazy);
-    int l0 = l, r0 = r;
-    for(l+=N, r+=N; l < r; l>>=1, r>>=1) {
-        if(l&1) { modify(l++,tree,lazy,val); }
-        if(r&1) { modify(--r,tree,lazy,val); }
-    } 
-    build(l0,tree,lazy); build(r0 - 1,tree,lazy);
-}
+    //Queries [L,R)
+    T query(int l, int r) {
+        push(l+N); push(r-1+N);
+        l += N; r += N;
+        T res1 = tree[l++];
+        if(l == r) return res1;
+        T res2 = tree[--r];
+        for(; l < r; l>>=1, r>>=1) {
+            if(l&1) res1 = merge(res1, tree[l++]);
+            if(r&1) res2 = merge(tree[--r], res2);
+        } 
+        return merge(res1, res2);
+    }
+
+    //Updates [L,R)
+    void update(int l, int r, U val) {
+        push(l+N); push(r-1+N);
+        int l0 = l, r0 = r;
+        for(l+=N, r+=N; l < r; l>>=1, r>>=1) {
+            if(l&1) { modify(l++,val); }
+            if(r&1) { modify(--r,val); }
+        } 
+        build(l0); build(r0 - 1);
+    }
+};
--- SegTree/Segtree (Range query + point update).cpp
+++ SegTree/Segtree (Range query + point update).cpp
@@ -1,39 +1,44 @@
-const int N = 1048576/2;
-
 struct node{
-    ll a;
-    node(ll aa=0) : a(aa) {}
+    ll left, right, pref, suf, store;
+    node(int a=0, int b=0,int c=0, int d=0, ll e=0) : left(a), right(b), pref(c), suf(d), store(e) {}
+    friend node merge(const node n1, const node n2) {
+        ll store = n1.store + n2.store + (n1.right - max(n1.left-1,n1.suf)) * (min(n2.right,n2.pref) - n2.left + 1);
+        return node(n1.left,n2.right,min(n2.pref, n1.pref),max(n1.suf, n2.suf),store);
+    }
 };
-node merge(const node& n1, const node& n2) {
-    return node(n1.a + n2.a);
-}
-template<typename T, typename U>
-void modify(T& n1, const U& n2) {
-    n1.a += n2;
-}
 
-template<typename T>
-inline void build(vector<T>& tree) {
-    int n = tree.size()>>1;
-    for(int i = n-1; i >= 0; i--) tree[i] = merge(tree[i<<1], tree[i<<1|1]);
-}
 
-//queries [L,R)
-template<typename T>
-T query(int l, int r, vector<T>& tree) {
-    int n = tree.size()>>1;
-    T res1 = tree[l+n]; l++;
-    if(l == r) return res1;
-    T res2 = tree[(--r)+n];  
-    for(l+=n, r+=n; l < r; l>>=1, r>>=1) {
-        if(l&1) res1 = merge(res1, tree[l++]);
-        if(r&1) res2 = merge(tree[--r], res2);
-    } 
-    return merge(res1, res2);
-}
+template<typename T> 
+struct SegTree {
+    int N, lg; vector<T> tree;
+    SegTree(int n, vector<T>& t) : tree(move(t)), N(n) { 
+        lg = 1; ll c = 1; 
+        while(c < n) { c<<=1; lg++; } 
+        for(int i = N-1; i > 0; i--) tree[i] = merge(tree[i<<1], tree[i<<1|1]);
+    }
 
-template<typename T>
-void update(int pos, int val, vector<T>& tree) {
-    int n = tree.size()>>1; pos+=n; modify(tree[pos], val);
-    for(pos>>=1; pos; pos>>=1) tree[pos] = merge(tree[pos<<1], tree[pos<<1|1]);
-}
+    inline void turnon(int pos) { 
+        tree[pos+N] = {pos, pos, pos, pos, 1};
+        pos+=N;
+        for(pos>>=1; pos; pos>>=1) tree[pos] = merge(tree[pos<<1], tree[pos<<1|1]);
+    }
+    
+    inline void turnoff(int pos) {
+        tree[pos+N] = {pos, pos, N, -1, 1};
+        pos+=N; 
+        for(pos>>=1; pos; pos>>=1) tree[pos] = merge(tree[pos<<1], tree[pos<<1|1]);
+    }
+
+    //Queries [L,R)
+    T query(int l, int r) {
+        l += N; r += N;
+        T res1 = tree[l++];
+        if(l == r) return res1;
+        T res2 = tree[--r];
+        for(; l < r; l>>=1, r>>=1) {
+            if(l&1) res1 = merge(res1, tree[l++]);
+            if(r&1) res2 = merge(tree[--r], res2);
+        } 
+        return merge(res1, res2);
+    }
+};
