b157befff25d98c9605d7403131b85270bdfb36b
Diptarag <diptarag1@gmail.com>
2021-09-17 22:41:11 +0530

    made sieve faster

--- Numbers/Linear Sieve.cpp
+++ Numbers/Linear Sieve.cpp
@@ -1,13 +1,16 @@
-template<int n = 1000000>
+template<int n = 100000000>
 struct lsieve {
     vector<int> sieve, primes;
     lsieve() {
         sieve.resize(n+1, 1);
         for(int i = 2; i <= n; ++i) {
             if(sieve[i]) primes.pb(i);
-            for(int j = 0; j < primes.size() && i*primes[j] <= n; ++j) {
-                sieve[i*primes[j]] = 0;
-                if(i%primes[j]==0) break;
+            int k = i*primes[0]; int j = 0;
+            while(1) {
+                if(k >= n) break;
+                sieve[k] = 0;
+                if(i%primes[j]==0 || j >= primes.size() - 1) break;
+                k = i*primes[++j];
             }
         }
     }
