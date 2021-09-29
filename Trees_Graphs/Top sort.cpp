99033238c593cb2fb7f1af6174350cc09adf5514
Diptarag <diptarag1@gmail.com>
2021-09-29 22:47:04 +0530

    Added cycle detection to top sort

--- Trees_Graphs/Top sort.cpp
+++ Trees_Graphs/Top sort.cpp
@@ -1,14 +1,19 @@
+int cyc = 0;
+
 void dfs(int node, vvi& g, vi& visited, vi& ans) {
+    	if(cyc) return;
 	visited[node] = 1;
 	for(int i : g[node]) {
-		if(visited[i]) continue;
+		if(visited[i] == 1) { cyc = 1; return; } 
+        	if(visited[i] == 2) continue;
 		dfs(i,g,visited,ans);
 	}
+    	visited[node] = 2;
 	ans.pb(node);
 }	
 
 vector<int> topsort(vvi& g) {
-	int n = g.size();
+	int n = g.size(); cyc = 0;
 	vector<int> ans;
 	vector<int> visited(n);
 	loop(i,0,n) {
