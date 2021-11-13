a081ebc4108284becd4af3f8a29765ef13566829
Diptarag <diptarag1@gmail.com>
2021-11-13 23:19:58 +0530

    Added multiplicative func to linear sieve

--- Numbers/Linear Sieve.cpp
+++ Numbers/Linear Sieve.cpp
@@ -1,16 +1,21 @@
 template<int n = 100000000>
 struct lsieve {
-    vector<int> sieve, primes;
+    vector<int> sieve, primes, phi;
     lsieve() {
         sieve.resize(n+1, 1);
+        phi.resize(n+1); phi[1] = 1;
         for(int i = 2; i <= n; ++i) {
-            if(sieve[i]) primes.pb(i);
-            int k = i*primes[0]; int j = 0;
-            while(1) {
-                if(k >= n) break;
-                sieve[k] = 0;
-                if(i%primes[j]==0 || j >= primes.size() - 1) break;
-                k = i*primes[++j];
+            if(sieve[i]) { 
+                primes.pb(i);
+                phi[i] = -1;
+            }
+            for (int j = 0; j < primes.size() && i * primes[j] <= n; ++j) {
+                sieve[i * primes[j]] = 0;
+                if(i%primes[j]==0) {
+                    phi[i * primes[j]] = 0; //change according to function
+                    break;
+                }
+                phi[i * primes[j]] = phi[i] * phi[primes[j]]; 
             }
         }
     }
