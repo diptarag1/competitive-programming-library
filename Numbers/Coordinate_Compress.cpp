bb64238312a9fffe9ac42dafbbc22dee9cf6c3e9
Diptarag <diptarag1@gmail.com>
2021-11-29 23:02:25 +0530

    Better coordinate compression

--- Numbers/Coordinate_Compress.cpp
+++ Numbers/Coordinate_Compress.cpp
@@ -1,9 +1,10 @@
-template<typename T> 
-int compress(vector<T> a, map<T, int>& id, int start = 0) {
-    sort(a.begin(), a.end());
-    int curid = start; id[a[0]] = curid++;
-    loop(i,1,a.size()) {
-        if(a[i] != a[i-1]) id[a[i]] = curid++;
-    }
-    return curid;
+template<typename T>
+int compress(vector<T>& a, vector<T>& d) {
+    d = a;
+    sort(d.begin(), d.end());
+    d.resize(unique(d.begin(), d.end()) - d.begin());
+    for (int i = 1; i < a.size(); ++i) {
+        a[i] = lower_bound(d.begin(), d.end(), a[i]) - d.begin();
+    } 
+    return d.size();
 }
