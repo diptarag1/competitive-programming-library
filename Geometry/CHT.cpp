72bcbfd23b9484a1bb2c81eb64cb27e3f58429fc
Diptarag <diptarag1@gmail.com>
2022-01-17 17:55:25 +0530

    structed CHT

--- Geometry/CHT.cpp
+++ Geometry/CHT.cpp
@@ -4,18 +4,26 @@ struct line {
     long double intersectX(line l) { return (long double) (c - l.c) / (l.m - m); }
 };
 
-vector<ll> ints(1e5+1);
-deque<line> dq;
-auto cmp = [](int idx, int x) { return dq[idx].intersectX(dq[idx + 1]) < x; };
-
-int query(int x) { //returns index of line in dq with maximum evaluation
-    return *lower_bound(ints.begin(), ints.begin() + dq.size() - 1, x, cmp);
+vector<int> init(int N) {
+    vector<int> ints(N);
+    iota(ints.begin(), ints.end(),0);
+    return ints;
 }
 
-void insert(line cur) { //insert works when slopes are in descending order
-    while (dq.size() >= 2 && cur.intersectX(dq[0]) >= dq[0].intersectX(dq[1])) dq.pop_front();
-    dq.push_front(cur);
-}
+struct CHT {
+    static vector<int> ints;
+    deque<line> dq;
+    CHT() { dq.push_back({0,0}); }
+    int query(int x) { //returns index of line in dq with maximum evaluation
+        return *lower_bound(ints.begin(), ints.begin() + dq.size() - 1, x, [&](int idx, int xx) { return dq[idx].intersectX(dq[idx + 1]) < xx; });
+    }
+    void insert(line cur) { //insert works when slopes are in descending order
+        while (dq.size() >= 2 && cur.intersectX(dq[0]) >= dq[0].intersectX(dq[1])) dq.pop_front();
+        dq.push_front(cur);
+    }
+};
+vector<int> CHT::ints = init(3e5);
 
 //if slopes are in ascending order, m -> -m, queryX -> -queryX
 //if min is needed, slope -> m -> -m, c -> -c
+	
